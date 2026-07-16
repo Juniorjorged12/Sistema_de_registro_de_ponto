a80fc1b3560582ab4b2c3c84d2b29caa33382c9d
juniorjorged12 <junior.jorged12@gmail.com>
2026-07-16 17:04:14 -0300

    melhorias no front end do sistema

--- registers/forms.py
+++ registers/forms.py
@@ -1,9 +1,33 @@
 from django import forms
 from .models import Funcionario
 
+
 class FuncionarioForm(forms.ModelForm):
     class Meta:
         model = Funcionario
-        # Campos que vão aparecer no formulário
         fields = ['nome_completo', 'cpf', 'cargo', 'salario', 'senha']
+        widgets = {
+            'nome_completo': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite o nome completo',
+            }),
+            'cpf': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': '000.000.000-00',
+            }),
+            'cargo': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex.: Analista',
+            }),
+            'salario': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'step': '0.01',
+                'min': '0',
+                'placeholder': '0.00',
+            }),
+            'senha': forms.PasswordInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite a senha',
+            }),
+        }
 
